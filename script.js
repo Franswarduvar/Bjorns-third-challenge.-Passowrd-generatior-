cb6992319a2db4ae4f40425c06acbfc9e8367c8b
B.O.A Addington <Franswarduvar>
2022-06-14 22:53:47 -0700

    commiting before I hvae my computer tomorrow

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
+
+
+var charnum = 8-128;
+var lowercase = true/false;
+var uppercase = true/false;
+var specialchar = true/false;
+var numbers = true/false;
+var Alow = ['a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'];
+
+function generatePassword(){
+  charnum = window.prompt("How many characters would you like?");
+  if (charnum < 8){
+    prompt ("invalid");
+  }
+  else if (charnum > 128) {
+    prompt("invalid");
+  }
+
+ lowercase = confirm("would you like lowercase characters?");
+ if (lowercase = true){
+  console.log("true");
+  }
+  for (var i = 0; i <= charnumLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+   }
+ uppercase = confirm("would you like upercase characters?");
+ if (uppercase = true){
+  console.log("true");
+  }
+  numbers = confirm("would you like numbers characters?");
+  if (numbers = true){
+   console.log("true");
+   }
+ specialchar = confirm("would you like specialchar characters?");
+ if (specialchar = true){
+  console.log("true");
+  }
+  return "ththth"
+}
+
+
+
+
+// function generatePassword(){
+// var numbers = "123456789";
+// var lowercase = "abcdefghijklmnopqrstuvwxyz";
+// var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// var characters = "!@#$%^&*()";
+
+// }
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -14,4 +67,4 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-window.prompt.addEventListener("How many characters would you like your password to be?");
+
